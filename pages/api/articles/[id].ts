63b87f80d539febe00cd5360c4394da36905b7e3
minatavakoli <minootavakoli28@gmail.com>
2022-09-27 16:25:33 +0330

    fix article item page

--- pages/api/articles/[id].ts
+++ pages/api/articles/[id].ts
@@ -4,7 +4,6 @@ import { NextApiResponse, NextApiRequest } from "next/types";
 
 const handler = ({ query: { id } }: NextApiRequest, res: NextApiResponse) => {
   const filtered = articles.filter((article) => article.id === id);
-
   if (filtered.length > 0) {
     res.status(200).json(filtered[0]);
   } else {
